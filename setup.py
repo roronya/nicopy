b63cafd1377b7f6ea46f5b6e71205c5fde82c1c9
roronya <yukikanai0204@gmail.com>
2016-02-04 23:25:12 +0900

    [modify] bug fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='nicopy',
-    version = '0.0.7',
+    version = '0.0.8',
     description='niconico api for python',
     long_description=long_description,
     url = 'http://github.com/roronya/nicopy',
